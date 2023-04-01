38aa0e3b28ef752d869e0a417f18f9430d707b76
Maksym Yeroshevych <makserosh95@gmail.com>
2023-04-01 23:08:24 +0300

    complete First Exercise

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,57 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const list = document.querySelector('.gallery');
+
+(function (){
+    const markup = galleryItems.map(({preview, original, description}) => 
+    `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </li>`)
+    list.insertAdjacentHTML('beforeend', markup.join(''))
+
+})()
+
+list.addEventListener('click', onclick);
+
+function onclick(evt){
+    evt.preventDefault();
+    
+    if(!evt.target.classList.contains('gallery__image')) {
+        return;
+    }
+    
+    const imgCard = evt.target.closest('.gallery__image');
+    const imgUrl = imgCard.dataset.source;
+    const data = galleryItems.find(({original}) => original === imgUrl);
+
+
+    const instance = basicLightbox.create(`
+    <img src="${data.original}" width="800" height="600">
+`,
+{
+  onShow: () => window.addEventListener("keydown", onCloseByEsc),
+  onClose: () => window.removeEventListener("keydown", onCloseByEsc),
+});
+
+
+const onCloseByEsc = (event) => {
+  if (event.code === "Escape") {
+    instance.close();
+  }
+}
+
+instance.show()
+
+}
+
+
+
